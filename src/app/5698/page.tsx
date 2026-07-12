f9514a55fcf335ac60a3b2b1fa3576d87516a589
benz862 <bombdon@gmail.com>
2026-07-12 10:11:23 -0400

    Add 5698 property widget page (#1)

---
+++ src/app/5698/page.tsx
@@ -0,0 +1,12 @@
+import Script from "next/script";
+
+export default function Property5698Page() {
+  return (
+    <Script
+      src="https://widgets.leadconnectorhq.com/loader.js"
+      data-resources-url="https://widgets.leadconnectorhq.com/chat-widget/loader.js"
+      data-widget-id="6a539e67c02522215a033cf3"
+      strategy="afterInteractive"
+    />
+  );
+}
